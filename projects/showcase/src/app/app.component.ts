45196b791a4560b2fdfccda3c0f9df94b58a6abd
Julia Körvers <j.koervers@makino.eu>
2024-11-13 13:52:11 +0100

    chore: add scripts to enhance the developer experience

--- projects/showcase/src/app/app.component.ts
+++ projects/showcase/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { ButtonComponent } from '@makino-software/design-system/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ButtonComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'showcase';

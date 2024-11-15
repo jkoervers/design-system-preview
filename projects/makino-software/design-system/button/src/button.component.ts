3df97e18c6ef46ee616d3a85585f3e8c62adac7e
Julia Körvers <j.koervers@makino.eu>
2024-11-15 14:32:46 +0100

    chore: add button component

--- projects/makino-software/design-system/button/src/button.component.ts
+++ projects/makino-software/design-system/button/src/button.component.ts
@@ -2,9 +2,17 @@ import { CommonModule } from '@angular/common';
 import { Component, HostBinding, input } from '@angular/core';
 
 import { ButtonColor } from './button-color.type';
-import { ButtonShape } from './button-shape.type';
-import { ButtonSize } from './button-size.type';
 import { ButtonVariant } from './button-variant.type';
+
+/**
+ * This is the button component. It can be used to build clicky things!
+ *
+ * [Figma reference](https://www.figma.com/file/X1JRLUCEp6JnoeKAxRPNeF/Angular-Architects-Design-Systems?node-id=1%3A3)
+ *  |
+ * [Implementation](https://github.com/andreaswissel/design-systems-workshop-latest/blob/lab-6/src/app/button/button.component.ts)
+ *  |
+ * [Specification](https://github.com/andreaswissel/design-systems-workshop/wiki/Lab-06:-Documentation)
+ */
 
 @Component({
   selector: 'button[iic-button]',
@@ -15,12 +23,10 @@ import { ButtonVariant } from './button-variant.type';
 })
 export class ButtonComponent {
   public readonly color = input<ButtonColor>('default');
-  public readonly shape = input<ButtonShape>('square');
-  public readonly size = input<ButtonSize>('medium');
   public readonly variant = input<ButtonVariant>('solid');
 
   @HostBinding('class')
   get classes(): string {
-    return `${this.color()} ${this.shape()} ${this.size()} ${this.variant()}`;
+    return `${this.color()} ${this.variant()}`;
   }
 }
---
+++ projects/makino-software/design-system/button/src/button.stories.ts
@@ -0,0 +1,50 @@
+import { argsToTemplate, type Meta, type StoryObj } from '@storybook/angular';
+
+import { ButtonComponent } from './button.component';
+
+const meta: Meta<ButtonComponent> = {
+  component: ButtonComponent,
+  title: 'ButtonComponent',
+  tags: ['autodocs'],
+  argTypes: {
+    color: {
+      options: ['default', 'blank', 'primary', 'secondary'],
+      control: { type: 'radio' },
+      table: {
+        defaultValue: { summary: 'default' },
+      },
+    },
+    variant: {
+      options: ['solid', 'outlined', 'flat'],
+      control: { type: 'radio' },
+      table: {
+        defaultValue: { summary: 'solid' },
+      },
+    },
+  },
+  render: ({ ...args }) => ({
+    props: args,
+    template: `
+      <button iic-button ${argsToTemplate(args)}>
+        Click me
+      </button>`,
+  }),
+};
+
+export default meta;
+type Story = StoryObj<ButtonComponent>;
+
+export const Primary: StoryObj<ButtonComponent> = {
+  name: 'Primary Button',
+  args: {
+    color: 'primary',
+    variant: 'solid',
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    ...Primary.args,
+    color: 'secondary',
+  },
+};
